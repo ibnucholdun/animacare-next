da711f468f58b6d4370db287f837aec3192efe9c
ibnucholdun <ibnucholdun023@gmail.com>
2024-02-26 14:34:55 +0700

    feat: add edit profile

--- src/components/fragments/Navbar/Index.tsx
+++ src/components/fragments/Navbar/Index.tsx
@@ -2,7 +2,7 @@ import Button from "@/components/ui/Button";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
 
 type Props = {};
@@ -11,8 +11,24 @@ const activeArtikel = ["artikel"];
 const activeForum = ["forum"];
 
 const Navbar = (props: Props) => {
-  const { data } = useSession();
+  const { data }: any = useSession();
   const { pathname } = useRouter();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const ref = useRef<any>(null);
+
+  useEffect(() => {
+    const handleOutSideClick = (event: any) => {
+      if (!ref.current?.contains(event.target)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    window.addEventListener("mousedown", handleOutSideClick);
+
+    return () => {
+      window.removeEventListener("mousedown", handleOutSideClick);
+    };
+  }, [ref]);
 
   return (
     <div className="flex md:flex-row justify-between items-center border-b-2 px-24">
@@ -59,12 +75,47 @@ const Navbar = (props: Props) => {
             }`}>
             Belanja
           </Link>
-          <Button
-            type="button"
-            className=" bg-blueLight px-4 text-white hover:bg-white hover:text-blueLight text-md"
-            onClick={() => (data ? signOut() : signIn())}>
-            {data ? "Logout" : "Login"}
-          </Button>
+          {data ? (
+            <div className="" ref={ref}>
+              <Button
+                type="button"
+                className="flex flex-row gap-2 items-center justify-center outline-none"
+                onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                <i className="bx bx-user text-xl"></i>
+                <p className="text-[16px]">{data?.user?.fullname}</p>
+                {isMenuOpen ? (
+                  <i className="bx bx-chevron-up text-xl"></i>
+                ) : (
+                  <i className="bx bx-chevron-down text-xl"></i>
+                )}
+              </Button>
+              {isMenuOpen && (
+                <div className={`absolute top-16 right-[70px]`}>
+                  <div className="w-[160px] h-[100px] bg-blueLight flex flex-col items-start justify-center rounded-lg border text-white">
+                    <Link
+                      href={"/member/profile"}
+                      className="flex flex-row gap-2 items-center justify-start hover:bg-white hover:text-blueLight hover:shadow-md px-2 py-1 rounded w-full ">
+                      <i className="bx bx-user-circle text-2xl"></i>
+                      <p>Lihat Profile</p>
+                    </Link>
+                    <Button
+                      className="flex flex-row gap-2 items-center justify-start hover:bg-white hover:text-blueLight hover:shadow-md px-2 py-1 rounded w-full outline-none"
+                      onClick={() => signOut()}>
+                      <i className="bx bx-log-out-circle text-2xl"></i>
+                      <p className="text-[16px]">Logout</p>
+                    </Button>
+                  </div>
+                </div>
+              )}
+            </div>
+          ) : (
+            <Button
+              type="button"
+              className=" bg-blueLight px-4 text-white hover:bg-white hover:text-blueLight text-md"
+              onClick={() => signIn()}>
+              Login
+            </Button>
+          )}
         </ul>
       </nav>
     </div>
---
+++ src/services/users/index.ts
@@ -0,0 +1,45 @@
+import instance from "@/lib/axios/instance";
+
+const userServices = {
+  //   getAllUsers: () => instance.get("/api/user"),
+  //   updateUser: (id: string, data: any, token: string) =>
+  //     instance.put(
+  //       "/api/user",
+  //       {
+  //         id,
+  //         data,
+  //       },
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       }
+  //     ),
+  //   deleteUser: (id: string, token: string) =>
+  //     instance.delete("/api/user", {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //       data: { id },
+  //     }),
+  getProfile: (token: string) =>
+    instance.get("/api/user/profile", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }),
+  updateProfile: (data: any, token: string) =>
+    instance.put(
+      "/api/user/profile",
+      {
+        data,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    ),
+};
+
+export default userServices;
